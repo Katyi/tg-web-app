554f4fa7062a3943e8de56a808a1b28182c1aabd
Alexandra Egorova <egorova.aleksandra@gmail.com>
2025-06-12 17:37:36 +0200

    added hook for tg

--- src/App.js
+++ src/App.js
@@ -1,14 +1,21 @@
 import { useEffect } from 'react';
+import { useTelegram } from './hooks/useTelegram';
 import './App.css';
-
-const tg = window.Telegram.WebApp;
+import Header from './components/Header/Header';
 
 function App() {
+  const { tg } = useTelegram();
+
   useEffect(() => {
     tg.ready();
   }, []);
 
-  return <div className="App">Working with Telegram Web App</div>;
+  return (
+    <div className="App">
+      <Header />
+      <h1>Telegram Web App</h1>
+    </div>
+  );
 }
 
 export default App;
