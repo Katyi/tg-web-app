0cdaa21ccdbdc65ebfe22e90cff2b24c2730c65f
Alexandra Egorova <egorova.aleksandra@gmail.com>
2025-06-13 10:32:31 +0200

    changed url for fetch in ProductList again

--- src/components/ProductList/ProductList.jsx
+++ src/components/ProductList/ProductList.jsx
@@ -71,7 +71,7 @@ const ProductList = () => {
       totalPrice: getTotalPrice(addedItems),
       queryId,
     };
-    fetch('http://localhost:8000', {
+    fetch('https://tg-web-app-node-pe3n5bu3s-katyis-projects.vercel.app', {
       method: 'POST',
       headers: {
         'Content-Type': 'application/json',
