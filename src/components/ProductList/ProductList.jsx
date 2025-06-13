04c70a105e61cdcd0e3bd987649fa8b4f157e8fd
Alexandra Egorova <egorova.aleksandra@gmail.com>
2025-06-13 12:14:34 +0200

    changed some code

--- src/components/ProductList/ProductList.jsx
+++ src/components/ProductList/ProductList.jsx
@@ -73,7 +73,8 @@ const ProductList = () => {
       totalPrice: getTotalPrice(addedItems),
       queryId: queryId,
     };
-    fetch('http://91.219.190.138:8000/web-data', {
+    // fetch('http://91.219.190.138:8000/web-data', {
+    fetch('http://localhost:8000/web-data', {
       method: 'POST',
       headers: {
         'Content-Type': 'application/json',
